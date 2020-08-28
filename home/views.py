012aeac48097b17a0e8cfb14f978ac48fa1ff9a3
ranjeet <thedigitalranjeet@gmail.com>
2020-08-29 03:32:00 +0530

    desktop view header add

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'home/index.html')
+    return render(request,'home/base.html')
 
 def product_details(request):
     return render(request,'home/product_details.html')
